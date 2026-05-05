779d70ee517a72e46fecf693c9155ef03e2b4db6
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2026-05-05 14:59:55 +0530

    fixed a little bit issue of the server and added frontend

--- 243701/index.js
+++ 243701/index.js
@@ -17,5 +17,5 @@ app.post("/notifications", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server running on port 3000 🚀");
 });
